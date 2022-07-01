a94bb8c7f08659f9b3d08fd2e2dc92aa36c4d274
eliasngumba <njugunangumba@gmail.com>
2022-07-01 10:58:03 -0700

    prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
    Use the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,17 +5,13 @@
 
 /**
  *
- * * main - print the string in the put function
+ *  * main - Entry point
  *
- * *
+ *   *
  *
- * * Description: using the main function
+ *    * Return: Always 0 (Success)
  *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+ *     */
 
 int main(void)
 
