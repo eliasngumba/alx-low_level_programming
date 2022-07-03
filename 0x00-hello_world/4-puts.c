70dd941d2afa9e07000e4c108ffbc29c5578b548
eliasngumba <njugunangumba@gmail.com>
2022-07-03 09:04:24 -0700

    prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
    Use the function puts
    
    You are not allowed to use printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
-
-
-
 /**
+ * main - entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (success)
+ */
 int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-}
+	puts("/"Programming is like building a multilingual puzzle");
+	Return (0);
