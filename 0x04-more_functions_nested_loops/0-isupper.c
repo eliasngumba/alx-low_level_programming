f77458c8b042fcc7a5b9c084a613998c8e520651
eliasngumba <njugunangumba@gmail.com>
2022-07-08 07:58:33 -0700

    task 3 lacks putchar.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,13 @@
 #include "main.h"
-/**
- * print_number - Prints a number
- * @n: The number to print
- */
-void print_number(int n)
-{
-	unsigned int num = n;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-	if (num > 9)
-	{
-		print_number(num / 10);
-	}
-	_putchar(num % 10 + '0');
+/**
+ * _isupper - function that verifies if a  character is uppercase or not
+ * @c: tested character
+ * Return: returns 1 if it is uppercase , 0 if not
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	return (0);
 }
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,7 +6,10 @@
  */
 int _isdigit(int c)
 {
-if ((c >= 48) && (c <= 57))
-return (1);
-return (0);
-{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
